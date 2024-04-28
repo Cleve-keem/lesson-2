c73fd561c73a3b764f9a7967a0b20891b7f199f1
cleve-keem <belloharyourmidey@gmail.com>
2024-04-28 12:02:00 -0700

    updating

--- public/script.js
+++ public/script.js
@@ -5,6 +5,7 @@ const openMenu = () => {
     const toggleMenu = () => {
         mobileMenu.classList.toggle('hidden');
         mobileMenu.classList.toggle('flex');
+        hamburgerBtn.classList.toggle('toggle-btn');
     };
     hamburgerBtn.addEventListener('click', toggleMenu);
     mobileMenu.addEventListener('click', toggleMenu);
